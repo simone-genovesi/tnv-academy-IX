6a1d1469d63f5739b275856322700a49ce797264
izzystitt <isabella.fip98@gmail.com>
2023-04-04 14:59:28 +0200

    correzioni esercizio js

--- the-net-pizza-html/pizze/isabella-pia/assets/scripts/index.js
+++ the-net-pizza-html/pizze/isabella-pia/assets/scripts/index.js
@@ -19,8 +19,6 @@ const renderSection = (sectionName, sectionId, piatti) => {
                         <p class="card-text">${piatto.description}</p>
                     </div>
                 </div>
-
-        
         `;
         sectionElement.appendChild(cardContainer);
     }
