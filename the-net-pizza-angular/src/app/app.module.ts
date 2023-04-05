510df1dbc265cacec1d83f906f7d8b59251861a8
Michele Fenu <michelefenu@gmail.com>
2023-04-05 17:29:34 +0200

    Add bootstrap and material

--- the-net-pizza-angular/src/app/app.module.ts
+++ the-net-pizza-angular/src/app/app.module.ts
@@ -3,16 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroContentComponent } from './hero-content/hero-content.component';
+import { MenuComponent } from './menu/menu.component';
+import { MenuSectionComponent } from './menu-section/menu-section.component';
+import { MenuItemComponent } from './menu-item/menu-item.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon'; 
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroContentComponent,
+    MenuComponent,
+    MenuSectionComponent,
+    MenuItemComponent,
+    FooterComponent,
+    ContactsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ the-net-pizza-angular/src/app/menu-section/menu-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-section',
+  templateUrl: './menu-section.component.html',
+  styleUrls: ['./menu-section.component.scss']
+})
+export class MenuSectionComponent {
+
+}
